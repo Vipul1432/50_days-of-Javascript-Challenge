c87baa72cc3ed6398d70197f61452e11324dae36
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-05-14 10:11:00 +0530

    Incrementing Counter

---
+++ Day15_Incrementing Counter/app.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const target = +counter.getAttribute('data-target');
+    const increment = target / 200;
+    const updateCounter = () => {
+        const c = +counter.innerText;
+
+        if (c < target) {
+            console.log(`c: ${c}, increment: ${increment}`);
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
